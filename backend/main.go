0417644cf4df82ff738ba3504419d57a2dc7162b
eDyrr <e6yr@proton.me>
2024-03-05 23:31:23 +0100

    wrote some more code, its dirty, smelly and not SOLID, but its fine, I'll work on it once the app is at least working

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,7 @@ type User struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Role     string `json:"role"`
 }
 
 type Permission struct {
@@ -26,6 +27,19 @@ type Ticket struct {
 	content  string `json:"content"`
 	owner    string `json:"owner_id"`
 	solution string `json:"solution_id"`
+	title    string `json:"title"`
+}
+
+type Solution struct {
+	ID       string `json:"ID"`
+	owner    string `json:"owner_id"`
+	ticket   string `json:"ticket_id"`
+	solution string `json:"solution"`
+}
+
+type Role struct {
+	ID   string `json:"ID"`
+	role string `json:"role"`
 }
 
 var db *sql.DB
@@ -54,9 +68,14 @@ func main() {
 	fmt.Print("connection success!\n")
 
 	gin.SetMode(gin.ReleaseMode)
+
 	router := gin.Default()
+	router.POST("/users", AddUser)
 	router.GET("/users", GetUsers)
+
+	router.POST("/tickets", AddTicket)
 	router.Run("localhost:8080")
+
 }
 
 func GetUsers(c *gin.Context) {
@@ -86,6 +105,136 @@ func GetTickets(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tickets)
 }
 
+func AddUser(c *gin.Context) {
+	var user User
+
+	if err := c.BindJSON(&user); err != nil {
+		return
+	}
+	id, err := insertUser(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	user.ID = fmt.Sprintf("%d", id)
+	c.JSON(http.StatusCreated, user)
+}
+
+func AddTicket(c *gin.Context) {
+	var ticket Ticket
+
+	if err := c.BindJSON(&ticket); err != nil {
+		return
+	}
+
+	_, err := insertTicket(ticket)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, ticket)
+}
+
+func AddSolution(c *gin.Context) {
+	var solution Solution
+	if err := c.BindJSON(&solution); err != nil {
+		return
+	}
+
+	if _, err := insertSolution(solution); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.IndentedJSON(http.StatusOK, solution)
+}
+
+func AddRole(c *gin.Context) {
+	var role Role
+	if err := c.BindJSON(&role); err != nil {
+		return
+	}
+	_, err := insertRole(role)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, role)
+}
+
+func AddPermission(c *gin.Context) {
+	var permission Permission
+	if err := c.BindJSON(&permission); err != nil {
+		return
+	}
+	_, err := insertPermission(permission)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, permission)
+}
+
+func insertPermission(permission Permission) (int64, error) {
+	result, err := db.Exec("INSERT INTO Permissions (permission) VALUES (?)", permission.permission)
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	return id, nil
+}
+
+func insertRole(role Role) (int64, error) {
+	result, err := db.Exec("INSERT INTO Roles (role) VALUES (?)", role.role)
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	return id, nil
+}
+func insertSolution(solution Solution) (int64, error) {
+	result, err := db.Exec("INSERT INTO Solutions (owner_id, ticket_id, solution) VALUES(?,?,?)", solution.owner, solution.ticket, solution.solution)
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	return id, nil
+}
+func insertTicket(ticket Ticket) (int64, error) {
+	result, err := db.Exec("INSERT INTO Tickets (content, owner_id, title) VALUES (?,?,?)", ticket.content, ticket.owner, ticket.title)
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	return id, nil
+}
+
+func insertUser(user User) (int64, error) {
+	result, err := db.Exec("INSERT INTO Users (username, email, password, role) VALUES (?, ?, ?, ?)", user.Username, user.Email, user.Password, user.Role)
+
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("error : %v", err)
+	}
+	return id, nil
+}
 func loadUsers(db *sql.DB) ([]User, error) {
 	Users := []User{}
 	rows, err := db.Query("SELECT * FROM Users ;")
@@ -95,7 +244,7 @@ func loadUsers(db *sql.DB) ([]User, error) {
 	defer rows.Close()
 	for rows.Next() {
 		var user User
-		if err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.Password); err != nil {
+		if err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.Role); err != nil {
 			return nil, fmt.Errorf("error : %v", err)
 		}
 		Users = append(Users, user)
